bce9a38b82a1e0d78c514b43f1ddae50c1182f5f
Engin Can Sürmeli <surmeli.engincan@gmail.com>
2024-06-07 10:26:44 +0300

    Refactor tabview layout and add frames for Home and Devices tabs

--- src/dashboard.py
+++ src/dashboard.py
@@ -13,13 +13,21 @@ class Dashboard(CTkTabview):
         self.add("Devices")
         self.add("Settings")
 
-        # remove segmented button
+        # remove segmented button of tabview
         self._top_spacing = 0
         self._top_button_overhang = 0
         self._segmented_button.grid_forget()
         self._configure_grid()
 
         # add frames on tabs
+        home_frame = CTkFrame(master=self.tab("Home"))
+        home_frame.pack(fill=BOTH, expand=True)
+
+        devices_frame = CTkFrame(master=self.tab("Devices"))
+        devices_frame.pack(fill=BOTH, expand=True)
+
+        # settings_frame = settings.SettingsFrame(master=self.tab("Settings"))
+        # settings_frame.pack(fill=BOTH, expand=True)
 
     def showTab(self, tab_name: str):
         self.set(tab_name)
